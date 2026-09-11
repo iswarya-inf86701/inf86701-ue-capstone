019f551b3fdfa21956b0883bf69f45b3bdb9a147
Iswarya4601 <slsiswaryavinjamuri@gmail.com>
2026-09-11 07:57:55 +0530

    deafult button

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -75,7 +75,19 @@ function buildAutoBlocks() {
  * @param {HTMLElement} main The main container element
  */
 export function decorateButtons(main) {
+  main.querySelectorAll('.button-container a[href]').forEach((a) => {
+    if (a.querySelector('img')) return;
+
+    const container = a.closest('.button-container');
+    a.className = 'button';
+    if (container.classList.contains('primary')) a.classList.add('primary');
+    if (container.classList.contains('secondary')) a.classList.add('secondary');
+    container.className = 'button-wrapper';
+  });
+
   main.querySelectorAll('p a[href]').forEach((a) => {
+    if (a.classList.contains('button')) return;
+
     a.title = a.title || a.textContent;
     const p = a.closest('p');
     const text = a.textContent.trim();
